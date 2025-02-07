808d88069e976f1aaf49c64127abb66e0d05eed7
Thiago França da Silva <tfsthiagobr98@outlook.com>
2025-02-07 14:34:53 -0300

    fix: treat graphql auth exception

--- src/Exceptions/KeycloakGuardException.php
+++ src/Exceptions/KeycloakGuardException.php
@@ -2,10 +2,34 @@
 
 namespace KeycloakGuard\Exceptions;
 
-class KeycloakGuardException extends \UnexpectedValueException
+use GraphQL\Error\ClientAware;
+use GraphQL\Error\ProvidesExtensions;
+
+class KeycloakGuardException extends \Exception implements ClientAware, ProvidesExtensions
 {
     public function __construct(string $message)
     {
         $this->message = "[Keycloak Guard] {$message}";
+    }
+
+    /**
+     * Returns true when exception message is safe to be displayed to a client.
+     *
+     * @api
+     */
+    public function isClientSafe(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Return the content that is put in the "extensions" part
+     * of the returned error.
+     */
+    public function getExtensions(): array
+    {
+        return [
+            'message' => $this->message,
+        ];
     }
 }
